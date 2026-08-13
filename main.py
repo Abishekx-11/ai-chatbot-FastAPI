4ad2245ba869f0216b73193ddcc1da85375e0b4b
Abishek Lamichhane <lamichhaneabishek276@gmail.com>
2026-08-13 22:08:05 +0545

    added in-memory chat history

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI , Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+
 from google import genai
 from dotenv import load_dotenv
 import os
@@ -17,6 +18,11 @@ templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+chat_history = []
+
+
+
 @app.get("/")
 def home(request:Request):
     return templates.TemplateResponse(
@@ -31,13 +37,31 @@ async def Chat(request:Request):
     request_data = await request.json()
     user_message = request_data["message"]
 
+    chat_history.append({               # appending user message in list as dictionary
+        "role": "user",
+        "content":user_message
+    })
+
+    conversation = ""           #created empty string 
+
+    for every_dict in chat_history:                     #created a conversation by appending every dictionary 
+        conversation += f"{every_dict["role"]}: {every_dict["content"]}\n"
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=user_message
+        contents=conversation                   #passing the conversation to AI
     )
 
+
+    ai_message = response.text 
+
+    chat_history.append({
+        "role":"AI",
+        "content":ai_message
+    })
+
     return {
-        "message": response.text
+        "message": ai_message
     }
 
 
