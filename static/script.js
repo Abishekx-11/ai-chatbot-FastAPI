2ba13ebdc56b693dc9e3dbf624a83203f1f67b71
Abishek Lamichhane <lamichhaneabishek276@gmail.com>
2026-08-15 06:01:34 +0545

    prevent duplicate chat submission

--- static/script.js
+++ static/script.js
@@ -1,20 +1,65 @@
 
+let isSending = false
+
 document.getElementById("first_form").addEventListener("submit", async function(event){
     event.preventDefault();
 
-    let userMessage = document.getElementById("chatting").value;
+    if(isSending){
+        return
+    }
+
+    let user = document.getElementById("chatting");
     let chatbox = document.getElementById("chat-box");
+    let sendButton = document.querySelector("#first_form button");
+
+
+    let userMessage = user.value.trim();
+
+    if (userMessage === "") {
+        return;
+    }
+
+    // Lock immediately
+    isSending = true
+
+
+     // Immediately clear the input
+    user.value = "";
+
+    // Prevent multiple submissions while waiting for Gemini
+    user.disabled = true;
+    sendButton.disabled = true;
+
     chatbox.innerHTML += `<p><strong>You: </strong> ${userMessage}</p>`;
 
-    let serverResponse = await fetch("/chat", {
-        method: "POST", 
-        headers: {"content-type":"application/json"},
-        body:JSON.stringify( {"message": userMessage})
-    });
 
-    let responseData = await serverResponse.json();
-    chatbox.innerHTML += `<p><strong>AI: </strong> ${responseData.message}</p>`;
+    try{
+        let serverResponse = await fetch("/chat", {
+            method: "POST", 
+            headers: {"content-type":"application/json"},
+            body:JSON.stringify( {"message": userMessage})
+        });
 
-    document.getElementById("chatting").value = "";
+        let responseData = await serverResponse.json();
+        chatbox.innerHTML += `<p><strong>AI: </strong> ${responseData.message}</p>`;
+    }
 
+    catch (error) {
+
+        chatbox.innerHTML += `<p><strong>AI:</strong> Something went wrong.</p>`;
+
+    } finally {
+
+        // release lock from form , that means, we can send anything again
+        isSending = false
+
+        // Allow the user to send another message
+        user.disabled = false;
+        sendButton.disabled = false;
+
+
+
+        // after Gemini responds, instead of you having to click the textbox again, the cursor is automatically ready there
+        user.focus();   
+    }
 });
